a2493ff6c7eaa043f2663ac4c4c94e5ef3fe7caa
Phillip Chang <lasagnaphil@snu.ac.kr>
2018-04-14 15:39:29 +0900

    currently working on fit to screen...

--- src/nodes/MeshNode.cpp
+++ src/nodes/MeshNode.cpp
@@ -2,6 +2,7 @@
 // Created by lasagnaphil on 2017-03-25.
 //
 
+#include <glm/ext.hpp>
 #include "MeshNode.h"
 
 MeshNode::MeshNode(std::shared_ptr<Mesh> mesh, Shader shader) :
@@ -30,4 +31,64 @@ void MeshNode::setShader(Shader shader) {
 
 void MeshNode::setMesh(std::shared_ptr<Mesh> mesh) {
     this->mesh = mesh;
+}
+
+MeshNode::BoundingBox MeshNode::findBoundingBox(glm::mat4 projViewMatrix) {
+    std::vector<glm::vec3> vertices = getAllVertices();
+    std::vector<glm::vec2> screenVertices;
+
+    std::transform(vertices.begin(), vertices.end(), std::back_inserter(screenVertices), [&](glm::vec3 pos) {
+        glm::vec4 result = projViewMatrix * glm::vec4(pos, 1.f);
+        return glm::vec2(result.x / result.w, result.y / result.w);
+    });
+
+    /*
+    printf("Screen vertices: \n");
+    for (auto& pos : screenVertices) {
+        printf("%f %f\n", pos.x, pos.y);
+    }
+     */
+    BoundingBox box {
+            .leftTop = screenVertices[0],
+            .rightBottom = screenVertices[0]
+    };
+
+    for (auto& pos : screenVertices) {
+        if (pos.x < box.leftTop.x) {
+            box.leftTop.x = pos.x;
+        } else if (pos.x > box.rightBottom.x) {
+            box.rightBottom.x = pos.x;
+        }
+        if (pos.y < box.leftTop.y) {
+            box.leftTop.y = pos.y;
+        } else if (pos.y > box.rightBottom.y) {
+            box.rightBottom.y = pos.y;
+        }
+    }
+    return box;
+}
+
+std::vector<glm::vec3> MeshNode::getAllVertices() {
+    std::vector<glm::vec3> vertices;
+
+    auto verticesOfMesh = meshToVertices();
+    vertices.insert(vertices.end(), verticesOfMesh.begin(), verticesOfMesh.end());
+
+    flat_transform(children.begin(), children.end(), std::back_inserter(vertices), [&](Node* child) {
+        auto meshChild = dynamic_cast<MeshNode*>(child);
+        if (meshChild) {
+            return meshChild->getAllVertices();
+        } else return std::vector<glm::vec3>();
+    });
+
+    return vertices;
+}
+
+std::vector<glm::vec3> MeshNode::meshToVertices() {
+    std::vector<glm::vec3> vertices;
+    std::transform(mesh->vertices.begin(), mesh->vertices.end(), std::back_inserter(vertices), [&](Vertex& vertex) {
+        auto result = worldTransform * glm::vec4(vertex.position, 1.f);
+        return glm::vec3(result);
+    });
+    return vertices;
 }
--- src/nodes/MeshNode.h
+++ src/nodes/MeshNode.h
@@ -19,5 +19,24 @@ public:
 
     std::shared_ptr<Mesh> mesh;
     Shader shader;
+
+    struct BoundingBox {
+        glm::vec2 leftTop;
+        glm::vec2 rightBottom;
+    };
+
+    BoundingBox findBoundingBox(glm::mat4 projViewMatrix);
+
+private:
+    template <typename InputIter, typename OutputIter, typename F>
+    void flat_transform(InputIter begin, InputIter end, OutputIter out, F fn) {
+        for (; begin != end; ++begin) {
+            auto y = fn(*begin);
+            std::copy(std::begin(y), std::end(y), out);
+        }
+    }
+
+    std::vector<glm::vec3> getAllVertices();
+    std::vector<glm::vec3> meshToVertices();
 };
 
