4e165732d36504d2447196ff1f7f7d125ffebd7c
Phillip Chang <lasagnaphil@snu.ac.kr>
2018-04-17 02:57:09 +0900

    omit khrplatform

--- deps/glad_gen.sh
+++ deps/glad_gen.sh
@@ -4,15 +4,15 @@ rm -rf glad
 
 case "$1" in
     release-3.3)
-        glad --profile=core --api="gl=3.3" --generator=c --out-path=glad
+        glad --profile=core --api="gl=3.3" --generator=c --omit-khrplatform --out-path=glad
         ;;
     release-4.3)
-        glad --profile=core --api="gl=4.3" --generator=c --out-path=glad
+        glad --profile=core --api="gl=4.3" --generator=c --omit-khrplatform --out-path=glad
         ;;
     debug-3.3)
-        glad --profile=core --api="gl=3.3" --generator=c-debug --extensions=GL_ARB_debug_output --out-path=glad
+        glad --profile=core --api="gl=3.3" --generator=c-debug --extensions=GL_ARB_debug_output --omit-khrplatform --out-path=glad
         ;;
     debug-4.3)
-        glad --profile=core --api="gl=4.3" --generator=c-debug --extensions=GL_ARB_debug_output --out-path=glad
+        glad --profile=core --api="gl=4.3" --generator=c-debug --extensions=GL_ARB_debug_output --omit-khrplatform --out-path=glad
         ;;
 esac
