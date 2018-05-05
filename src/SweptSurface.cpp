e72b493f7a941db8cb6b2fb1fd8ae5a6b14841ae
Phillip Chang <lasagnaphil@snu.ac.kr>
2018-05-05 16:54:09 +0900

    initial render (pretty horrible)

--- src/SweptSurface.cpp
+++ src/SweptSurface.cpp
@@ -2,18 +2,31 @@
 // Created by lasagnaphil on 5/1/18.
 //
 
+#include <string>
+
 #include "SweptSurface.h"
 
-std::shared_ptr<Mesh> SweptSurface::constructFromFile(const char* filename) {
+std::string rtrim(std::string& str) {
+    str.erase(str.find_last_not_of(" \n\r\t")+1);
+    return str;
+}
+
+void getline(std::ifstream& istream, std::string& line) {
+    while (true) {
+        std::getline(istream, line);
+        if (!rtrim(line).empty()) break;
+    }
+}
+
+std::shared_ptr<Mesh> SweptSurface::constructFromFile(const char* filename, std::shared_ptr<Material> material) {
     using std::cerr;
     using std::endl;
 
     std::ifstream istream(filename);
     std::string line;
+
     // get title
-    if (!std::getline(istream, line)) {
-        std::cerr << "File empty!" << std::endl;
-    }
+    getline(istream, line);
 
     if (line != "BSPLINE") {
         std::cerr << "Sorry, we only allow BSPLINE" << std::endl;
@@ -21,9 +34,9 @@ std::shared_ptr<Mesh> SweptSurface::constructFromFile(const char* filename) {
     int numCrossSections = 0;
     int numControlPoints = 0;
 
-    std::getline(istream, line);
+    getline(istream, line);
     numCrossSections = std::stoi(line);
-    std::getline(istream, line);
+    getline(istream, line);
     numControlPoints = std::stoi(line);
 
     std::vector<Polygon2D> polygons;
@@ -34,31 +47,36 @@ std::shared_ptr<Mesh> SweptSurface::constructFromFile(const char* filename) {
         Polygon2D polygon;
         for (int i = 0; i < numControlPoints; ++i) {
             glm::vec2 pos;
-            std::getline(istream, line);
+            getline(istream, line);
             std::istringstream(line) >> pos.x >> pos.y;
             polygon.push_back(pos);
         }
-        std::getline(istream, line);
+        getline(istream, line);
         float scale = std::stof(line);
         transform.scale = {scale, scale, scale};
-        std::getline(istream, line);
-        std::istringstream(line) >> transform.rotation.w
-                                 >> transform.rotation.x
+        getline(istream, line);
+        std::istringstream(line) >> transform.rotation.x
                                  >> transform.rotation.y
-                                 >> transform.rotation.z;
-        std::getline(istream, line);
+                                 >> transform.rotation.z
+                                 >> transform.rotation.w;
+        getline(istream, line);
         std::istringstream(line) >> transform.position.x
                                  >> transform.position.y
                                  >> transform.position.z;
-
         polygons.push_back(polygon);
         transforms.push_back(transform);
     }
-    return construct(polygons, transforms, numControlPoints, numCrossSections);
+    return construct(polygons, transforms, std::move(material), numControlPoints, numCrossSections);
+}
+
+glm::vec3 normalOfTriangle(const glm::vec3& v1, const glm::vec3& v2, const glm::vec3& v3) {
+    return glm::normalize(glm::cross(v2 - v1, v3 - v1));
 }
 
 std::shared_ptr<Mesh>
-SweptSurface::construct(std::vector<SweptSurface::Polygon2D> controlPolygons, std::vector<Transform> polygonTransforms,
+SweptSurface::construct(std::vector<SweptSurface::Polygon2D> controlPolygons,
+                        std::vector<Transform> polygonTransforms,
+                        std::shared_ptr<Material> material,
                         int numControlPoints, int numCrossSections) {
 
     assert(controlPolygons.size() == numCrossSections);
@@ -67,40 +85,39 @@ SweptSurface::construct(std::vector<SweptSurface::Polygon2D> controlPolygons, st
     int interpPolygonSize = numControlPoints * (1 << INTERP_FACTOR);
     std::vector<Polygon2D> bsplines;
     for (auto polygon : controlPolygons) {
-        polygon.resize(interpPolygonSize);
-        Polygon2D otherPolygon;
-        otherPolygon.resize(interpPolygonSize);
-        Polygon2D* prevP = &otherPolygon;
-        Polygon2D* nextP = &polygon;
-
+        Polygon2D prev;
+        Polygon2D next = polygon;
         for (int iter = 0; iter < INTERP_FACTOR; iter++) {
-            Polygon2D* temp = prevP;
-            prevP = nextP;
-            nextP = temp;
-            for (int i = 0; i < numControlPoints * (1 << iter); i += 4) {
-                auto& next = *nextP;
-                auto& prev = *prevP;
-                next[2*i] = (prev[i] + prev[i+1]) / 2.f;
-                next[2*i+1] = (prev[i] + 6.f * prev[i+1] + prev[i+2]) / 8.f;
-                next[2*i+2] = (prev[i+1] + prev[i+2]) / 2.f;
-                next[2*i+3] = (prev[i+1] + 6.f * prev[i+2] + prev[i+3]) / 8.f;
-                next[2*i+4] = (prev[i+2] + prev[i+3]) / 2.f;
-                next[2*i+5] = (prev[i+2] + 6.f * prev[i+2] + prev[i+3]) / 8.f;
-                next[2*i+6] = (prev[i+3] + prev[i]) / 2.f;
-                next[2*i+7] = (prev[i+3] + 6.f * prev[i+3] + prev[i]) / 8.f;
+            prev = next;
+            int numVertices = numControlPoints * (1 << iter);
+            next.clear();
+            next.resize(numVertices * 2);
+            for (int i = 0; i < numVertices; ++i) {
+                int iprev = (i - 1) % numVertices;
+                int inext = (i + 1) % numVertices;
+                next[2*i] = (prev[iprev] + 6.f * prev[i] + prev[inext]) / 8.f;
+                next[2*i+1] = (prev[i] + prev[inext]) / 2.f;
             }
-
         }
-        Polygon2D result = *nextP;
-        bsplines.push_back(result);
+        bsplines.push_back(next);
     }
 
-    std::vector<WireframeVertex> vertices;
-    std::vector<WireframeVertex> verticeBuffer1, verticeBuffer2;
-    verticeBuffer1.reserve(interpPolygonSize);
-    verticeBuffer2.reserve(interpPolygonSize);
-    std::vector<WireframeVertex>* prevVertices = &verticeBuffer1;
-    std::vector<WireframeVertex>* nextVertices = &verticeBuffer2;
+    for (auto& bspline : bsplines) {
+        std::cout << std::endl;
+        for (auto& pos : bspline) {
+            std::cout << pos.x << " " << pos.y << std::endl;
+        }
+    }
+
+    //
+    // Calculate the final B-spline positions
+    //
+    int finalBsplineCount = (numCrossSections - 1) * TRANSFORM_INTERP_SIZE + 1;
+    std::vector<std::vector<Vertex>> finalBsplines(finalBsplineCount);
+
+    for (auto& v : finalBsplines) {
+        v.reserve(interpPolygonSize);
+    }
 
     for (int i = 0; i < numCrossSections - 1; ++i) {
         auto& tprev = polygonTransforms[i];
@@ -110,42 +127,91 @@ SweptSurface::construct(std::vector<SweptSurface::Polygon2D> controlPolygons, st
 
         for (int j = 0; j < TRANSFORM_INTERP_SIZE; ++j) {
             Transform transform;
-            float alpha = (float)(j) / TRANSFORM_INTERP_SIZE;
+            float alpha = (float) (j) / TRANSFORM_INTERP_SIZE;
             transform.position = alpha * tprev.position + (1 - alpha) * tnext.position;
             transform.rotation = glm::slerp(tprev.rotation, tnext.rotation, alpha);
             transform.scale = alpha * tprev.scale + (1 - alpha) * tnext.scale;
 
             for (auto& pos : bspline) {
-                WireframeVertex vertex;
-                vertex.position = transform.toMat4() * glm::vec4(pos.x, pos.y, 0.0f, 0.0f);
-                nextVertices->push_back(vertex);
-            }
-
-            if (j > 0) {
-                // using prev and next vertices, we create the triangle strip
-                for (int k = 0; k < interpPolygonSize; ++k) {
-                    int kp = (k + 1) % interpPolygonSize;
-                    WireframeVertex v1 = (*prevVertices)[k];
-                    v1.barycentric = {1.0f, 0.0f, 0.0f};
-                    WireframeVertex v2 = (*prevVertices)[kp];
-                    v2.barycentric = {0.0f, 1.0f, 0.0f};
-                    WireframeVertex v3 = (*nextVertices)[k];
-                    v3.barycentric = {0.0f, 0.0f, 1.0f};
-                    WireframeVertex v4 = (*prevVertices)[kp];
-                    v4.barycentric = {1.0f, 0.0f, 0.0f};
-                    WireframeVertex v5 = (*prevVertices)[kp];
-                    v5.barycentric = {0.0f, 1.0f, 0.0f};
-                    WireframeVertex v6 = (*nextVertices)[k];
-                    v6.barycentric = {0.0f, 0.0f, 1.0f};
-                    vertices.insert(vertices.end(), {v1, v2, v3, v4, v5, v6});
-                }
+                Vertex vertex;
+                vertex.position = transform.toMat4() * glm::vec4(pos.x, pos.y, 0.0f, 1.0f);
+                finalBsplines[i * TRANSFORM_INTERP_SIZE + j].push_back(vertex);
             }
         }
-        auto temp = prevVertices;
-        prevVertices = nextVertices;
-        nextVertices = temp;
     }
 
-    std::shared_ptr<Mesh> mesh(new Mesh(vertices));
+    // Don't forget to include the last one!
+    auto& lastBspline = bsplines[numCrossSections - 1];
+    auto& lastTransform = polygonTransforms[numCrossSections - 1];
+    for (auto& pos : lastBspline) {
+        Vertex vertex;
+        vertex.position = lastTransform.toMat4() * glm::vec4(pos.x, pos.y, 0.0f, 0.0f);
+        finalBsplines[finalBsplineCount - 1].push_back(vertex);
+    }
+
+    for (auto& bspline : finalBsplines) {
+        std::cout << std::endl;
+        for (auto& vertex : bspline) {
+            std::cout << vertex.position.x << " " << vertex.position.y << " " << vertex.position.z << " " << std::endl;
+        }
+    }
+
+    //
+    // Find all the normals of the bspline vertices
+    //
+    for (int i = 0; i < finalBsplineCount; ++i) {
+        for (int j = 0; j < interpPolygonSize; ++j) {
+            int jnext = (j + 1) % interpPolygonSize;
+            int jprev = (j - 1) % interpPolygonSize;
+            finalBsplines[i][j].normal = {0.0f, 0.0f, 0.0f};
+            if (i != finalBsplineCount - 1) {
+                finalBsplines[i][j].normal += normalOfTriangle(
+                        finalBsplines[i][j].position,
+                        finalBsplines[i + 1][j].position,
+                        finalBsplines[i][jnext].position
+                );
+                finalBsplines[i][j].normal += normalOfTriangle(
+                        finalBsplines[i][j].position,
+                        finalBsplines[i][jprev].position,
+                        finalBsplines[i + 1][j].position
+                );
+            }
+            if (i != 0) {
+                finalBsplines[i][j].normal += normalOfTriangle(
+                        finalBsplines[i][j].position,
+                        finalBsplines[i][jnext].position,
+                        finalBsplines[i - 1][j].position
+                );
+                finalBsplines[i][j].normal += normalOfTriangle(
+                        finalBsplines[i][j].position,
+                        finalBsplines[i - 1][j].position,
+                        finalBsplines[i][jprev].position
+                );
+            }
+            finalBsplines[i][j].normal = glm::normalize(finalBsplines[i][j].normal);
+        }
+    }
+
+    // create the final vertices list
+    std::vector<Vertex> vertices;
+    for (int i = 0; i < finalBsplineCount - 1; ++i) {
+        for (int j = 0; j < interpPolygonSize; ++j) {
+            int jnext = (j + 1) % interpPolygonSize;
+            auto& v1 = finalBsplines[i][j];
+            auto& v2 = finalBsplines[i+1][j];
+            auto& v3 = finalBsplines[i][jnext];
+            auto& v4 = finalBsplines[i+1][jnext];
+            v1.texCoords = {0.0f, 0.0f}; v2.texCoords = {1.0f, 0.0f}; v3.texCoords = {0.0f, 1.0f}; v4.texCoords = {1.0f, 1.0f};
+            vertices.insert(vertices.end(), {v1, v2, v3, v2, v3, v4});
+        }
+    }
+
+    /*
+    for (auto& vertex : vertices) {
+        std::cout << vertex.position.x << " " << vertex.position.y << " " << vertex.position.z << " " << std::endl;
+    }
+     */
+
+    std::shared_ptr<Mesh> mesh(new Mesh(vertices, {}, std::move(material), GL_TRIANGLES, false));
     return mesh;
 }
--- src/SweptSurface.h
+++ src/SweptSurface.h
@@ -12,16 +12,18 @@
 
 class SweptSurface {
 public:
-    static constexpr int INTERP_FACTOR = 4;
-    static constexpr int TRANSFORM_INTERP_SIZE = 4;
+    static constexpr int INTERP_FACTOR = 2;
+    static constexpr int TRANSFORM_INTERP_SIZE = 16;
 
     using Polygon2D = std::vector<glm::vec2>;
     using Polygon3D = std::vector<glm::vec3>;
 
-    static std::shared_ptr<Mesh> constructFromFile(const char* filename);
+    static std::shared_ptr<Mesh> constructFromFile(const char* filename, std::shared_ptr<Material> material);
 
-    static std::shared_ptr<Mesh> construct(std::vector<SweptSurface::Polygon2D> controlPolygons, std::vector<Transform> polygonTransforms,
-                                               int numControlPoints, int numCrossSections);
+    static std::shared_ptr<Mesh> construct(std::vector<SweptSurface::Polygon2D> controlPolygons,
+                                           std::vector<Transform> polygonTransforms,
+                                           std::shared_ptr<Material> material,
+                                           int numControlPoints, int numCrossSections);
 };
 
 
