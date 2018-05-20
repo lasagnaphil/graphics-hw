3181ba4cefa5d6252866d9f85b94eb192dd09279
Phillip Chang <lasagnaphil@snu.ac.kr>
2018-05-21 01:39:25 +0900

    Fixed weird clipping issue (because of lighting)
    - Forgot to assign shininess parameter in Material::Material()

--- src/Material.cpp
+++ src/Material.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "Material.h"
+
+Material::Material(glm::vec4 ambient, glm::vec4 diffuse, glm::vec4 specular, float shininess)
+        : ambient(ambient), diffuse(diffuse), specular(specular), shininess(shininess) {
+    type = Type::SingleColor;
+}
+
+Material::Material(std::vector<Texture> textures, float shininess)
+        : textures(std::move(textures)), shininess(shininess) {
+    type = Type::Textured;
+}
+
+std::shared_ptr<Material> Material::create(std::initializer_list<Texture> textures, float shininess) {
+    return std::shared_ptr<Material>(new Material(textures, shininess));
+}
+
+std::shared_ptr<Material> Material::create(glm::vec4 ambient, glm::vec4 diffuse, glm::vec4 specular, float shininess) {
+    return std::shared_ptr<Material>(new Material(ambient, diffuse, specular, shininess));
+}
